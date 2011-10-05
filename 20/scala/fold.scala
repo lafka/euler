e8fce278eb593e0a13fb45624b6741a85f556b3f
Olav Frengstad <olav@fwt.no>
2011-10-05 21:12:19 +0200

    Whitespace

--- 20/scala/fold.scala
+++ 20/scala/fold.scala
@@ -1,4 +1,3 @@
-val x = (1 until 100).foldLeft((1:BigInt))( _ * _ ).toString.map{ c => c.toString.toInt }.foldLeft(0)(_ + _)
+val x = (1 until 100).foldLeft((1:BigInt))( _ * _ ).toString.map{ c => c.toString.toInt }.foldLeft(0)( _ + _ )
 
 println( "Here you go: %d".format(x) )
- 
